c97081fbf4cf20ff95bcc92300fcb3dfe7fa7a85
Lautaro Viola <lviola@alumno.huergo.edu.ar>
2021-08-29 18:56:43 -0300

    Menu version 1.0 terminado

--- assets/menu/menu.py
+++ assets/menu/menu.py
@@ -2,17 +2,17 @@ import pygame
 import ctypes 
 import sys
 
-
 pygame.init()
 user32 = ctypes.windll.user32
 user32.SetProcessDPIAware()
 ancho, alto = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
 
+# Resolucion y Pantalla
 resolucion = (ancho, alto)
 pantalla = pygame.display.set_mode((resolucion), pygame.RESIZABLE)
-pygame.FULLSCREEN
 
 
+# Fondo
 fondo = pygame.image.load("assets/fondos/fondo1.png")
 fondo = pygame.transform.scale(fondo, (ancho, alto))
 rect_fondo = fondo.get_rect()
@@ -20,50 +20,55 @@ rect_fondo = rect_fondo.move((0, 0))
 pantalla.blit(fondo, rect_fondo)
 
 
+# Icono y nombre de ventana
+icono = pygame.image.load("assets/menu/imagenesMenu/icono.png")
+pygame.display.set_icon(icono)
+pygame.display.set_caption('Ultramon')
+
 # Colores
-
-COLOR_BLANCO = (255,255,255)   
-COLOR_LIGHT = (170,170,170)
-COLOR_NEGRO = (100,100,100)
-COLOR_AZUL = (20, 25, 60)
-
-TIPOGRAFIA = pygame.font.SysFont('Corbel', 30) 
+COLOR_CIAN = (162,202,223)   
+COLOR_CELESTE = (178,255,255)
 
 
+# Boton Salir (Diseño)
 texto_salir = pygame.image.load("assets/menu/imagenesMenu/salirEstado1.png")
-pantalla.blit(texto_salir, (ancho/2, alto/2))
-texto_salir = pygame.transform.scale(texto_salir, (200, 100))
+texto_salir = pygame.transform.scale(texto_salir, (400, 250))
+pantalla.blit(texto_salir, (ancho/2 - 200, alto/2 - 125))
 
-
-texto_jugar = TIPOGRAFIA.render('Jugar', True, COLOR_BLANCO)
+# Boton Jugar (Diseño)
+texto_jugar = pygame.image.load("assets/menu/imagenesMenu/jugarEstado1.png")
+texto_jugar = pygame.transform.scale(texto_jugar, (400, 250))
+pantalla.blit(texto_jugar, (ancho/2 - 200, alto/3 - 125))
 
 
 while True: 
       
-    for ev in pygame.event.get(): 
+    for ev in pygame.event.get():
         if ev.type == pygame.QUIT: 
             pygame.quit()
         if ev.type == pygame.MOUSEBUTTONDOWN: 
             if ancho/2 <= mouse[0] <= ancho/2+140 and alto/2 <= mouse[1] <= alto/2+40: 
                 pygame.quit() 
-    
+                
     mouse = pygame.mouse.get_pos() 
 
 
-    # Boton Salir  
-    if ancho/2 <= mouse[0] <= ancho/2+50 and alto/2 <= mouse[1] <= alto/2+50: 
+    # Boton Salir (Funcion)
+
+    if ancho/2-100 <= mouse[0] <= ancho/2+100 and alto/2-100 <= mouse[1] <= alto/2+100: 
         texto_salir = pygame.image.load("assets/menu/imagenesMenu/salirEstado1.png")
-    else: 
+        # pygame.draw.rect(pantalla, COLOR_CELESTE,[ancho/2-125, alto/2-75, 250, 150])
+    else:
         texto_salir = pygame.image.load("assets/menu/imagenesMenu/salirEstado2.png")
+        # pygame.draw.rect(pantalla, COLOR_CIAN, [ancho/2-125, alto/2-75, 250, 150])
 
 
-    # Boton Jugar
-    if ancho/2 <= mouse[0] <= ancho/2+50 and alto/3 <= mouse[1] <= alto/3+50: 
-        pygame.draw.rect(pantalla, COLOR_LIGHT,[ancho/2-50, alto/3, 100, 50]) 
-    else: 
-        pygame.draw.rect(pantalla, COLOR_NEGRO, [ancho/2-50, alto/3, 100, 50])
-    pantalla.blit(texto_jugar, (ancho/2-50, alto/3))
+    # Boton Jugar (Funcion)
 
-      
+    if ancho/2-20 <= mouse[0] <= ancho/2+20 and alto/3-20 <= mouse[1] <= alto/3+20: 
+        texto_jugar = pygame.image.load("assets/menu/imagenesMenu/jugarEstado1.png")
+    else:
+        texto_jugar = pygame.image.load("assets/menu/imagenesMenu/jugarEstado2.png")
+
 
     pygame.display.update()
