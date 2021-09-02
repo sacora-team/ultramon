06a86cb52d95e7ec4c68630e22de592aac5292eb
Laucha23 <laucha.viola@gmail.com>
2021-09-01 21:12:23 -0300

    Reconocer SO

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from src.AppController import AppController
+from platform import system
+import ctypes
+from src.constantes.sistemasOperativos import SistemasOperativos
 
-# TODO: Preguntar si es windows OS
-# appid = u"ultramon"
-# ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(appid)
+sistema = system()
+
+if sistema == SistemasOperativos.WINDOWS:
+    appid = u"ultramon"
+    ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(appid)
+
 
 if __name__ == "__main__":
     AppController().iniciarJuego()
