3e3d35841b6854eb4a33e74376e7f5ac14a4d8f1
galobponce <personal.galo.bello@gmail.com>
2021-09-04 00:54:41 +0000

    agrego view y controller de menu

--- src/entidades/menu/MenuController.py
+++ src/entidades/menu/MenuController.py
@@ -1,32 +1,60 @@
+from pygame import quit
 from ..base.ControllerBase import ControllerBase
 from .MenuView import MenuView
+
 
 class MenuController(ControllerBase):
     def __init__(self) -> None:
         super().__init__()
         self.activado: bool = False
         self.view = MenuView()
-    
+        self.estadoFlecha = "JUGAR"
+
     def getView(self) -> MenuView:
         return self.view
-    
+
     def getActivado(self) -> bool:
         return self.activado
-    
+
     def setActivado(self, activado: bool) -> None:
         self.activado = activado
-    
+
+    def getEstadoFlecha(self):
+        return self.estadoFlecha
+
+    def setEstadoFlecha(self, estado: str):
+        self.estadoFlecha = estado.upper()
+
     def loop(self) -> None:
-        self.getView().renderizarMenuPrincipal()
+        self.getView().renderizarMenuPrincipal("JUGAR")
         while self.getActivado():
             self.chequearEventos()
             if self.getUpKey():
-                print("arriba")
+                self.moverFlecha("ARRIBA")
                 self.resetearKeys()
             if self.getDownKey():
-                print("abajo")
+                self.moverFlecha("ABAJO")
                 self.resetearKeys()
-    
+            if self.getSelectKey():
+                self.resetearKeys()
+                if self.getEstadoFlecha() == "SALIR":
+                    quit()
+                elif self.getEstadoFlecha() == "JUGAR":
+                    self.setActivado(False)
+
+    def moverFlecha(self, movimiento: str):
+        if movimiento.upper() == "ARRIBA":
+            if self.getEstadoFlecha() == "JUGAR":
+                self.setEstadoFlecha("SALIR")
+            elif self.getEstadoFlecha() == "SALIR":
+                self.setEstadoFlecha("JUGAR")
+        elif movimiento.upper() == "ABAJO":
+            if self.getEstadoFlecha() == "JUGAR":
+                self.setEstadoFlecha("SALIR")
+            elif self.getEstadoFlecha() == "SALIR":
+                self.setEstadoFlecha("JUGAR")
+        self.getView().renderizarMenuPrincipal(self.getEstadoFlecha())
+
     def activar(self) -> None:
         self.setActivado(True)
         self.loop()
--- src/entidades/menu/MenuView.py
+++ src/entidades/menu/MenuView.py
@@ -1,18 +1,65 @@
 from ..base.ViewBase import ViewBase
 
+
 class MenuView(ViewBase):
     def __init__(self) -> None:
         super().__init__()
         self.renderizado: bool = False
+        self.offsetFlecha: int = -100
+        self.alturasFlecha: dict[str, int] = {
+            "JUGAR": self.getAlto() // 3 - 60,
+            "SALIR": self.getAlto() // 2 - 40,
+        }
 
     def getEstado(self) -> bool:
         return self.renderizado
-    
+
     def setEstado(self, estado: bool):
         self.renderizado = estado
 
-    def renderizarMenuPrincipal(self) -> None:
-        self.renderizarImagen("assets/fondos/fondo1.png", 0, 0, (self.getAncho(), self.getAlto()))
-        self.renderizarImagen("assets/menu/imagenesMenu/jugarEstado1.png", self.getAncho() // 2 - 200, self.getAlto() // 3 - 125, (400, 250))
-        self.renderizarImagen("assets/menu/imagenesMenu/salirEstado1.png", self.getAncho() // 2 - 200, self.getAlto() // 2 - 90, (400, 180))
-        
+    def getOffsetFlecha(self) -> int:
+        return self.offsetFlecha
+
+    def getAlturasFlecha(self) -> dict[str, int]:
+        return self.alturasFlecha
+
+    def setAlturaFlechaActual(self, altura: str) -> None:
+        self.alturaFlechaActual = altura
+
+    def renderizarMenuPrincipal(self, alturaFlecha: str) -> None:
+        self.renderizarImagen(
+            "assets/fondos/fondo1.png", 0, 0, (self.getAncho(), self.getAlto())
+        )
+        self.renderizarImagen(
+            "assets/menu/arrow.png",
+            self.getAncho() // 2 - 200 + self.getOffsetFlecha(),
+            self.getAlturasFlecha()[alturaFlecha],
+            (150, 100),
+        )
+
+        if alturaFlecha == "JUGAR":
+            self.renderizarImagen(
+                "assets/menu/imagenesMenu/jugarEstado2.png",
+                self.getAncho() // 2 - 200,
+                self.getAlto() // 3 - 125,
+                (400, 250),
+            )
+            self.renderizarImagen(
+                "assets/menu/imagenesMenu/salirEstado1.png",
+                self.getAncho() // 2 - 200,
+                self.getAlto() // 2 - 90,
+                (400, 200),
+            )
+        elif alturaFlecha == "SALIR":
+            self.renderizarImagen(
+                "assets/menu/imagenesMenu/jugarEstado1.png",
+                self.getAncho() // 2 - 200,
+                self.getAlto() // 3 - 125,
+                (400, 250),
+            )
+            self.renderizarImagen(
+                "assets/menu/imagenesMenu/salirEstado2.png",
+                self.getAncho() // 2 - 200,
+                self.getAlto() // 2 - 90,
+                (400, 200),
+            )
