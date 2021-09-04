780f97a2e69b23078fc2d2c70f0f5fa9650d1e61
Laucha23 <lviola@alumno.huergo.edu.ar>
2021-09-04 12:43:01 -0300

    errores en movimiento personaje

--- src/entidades/juego/ashPrueba.py
+++ src/entidades/juego/ashPrueba.py
@@ -11,152 +11,226 @@ icono = pygame.image.load('assets/items/ultraball.png')
 pygame.display.set_icon(icono)
 
 # Fondo del juego
-fondo = pygame.image.load('assets/fondos/fondo1.png')
+fondo = pygame.image.load('assets/fondos/fondoPNG.png')
 
-# Sprites Ash
-ash = pygame.image.load('assets/personajes/ash/frente.png')
-
-caminaDerecha = [
-        pygame.image.load('assets/personajes/ash/derecha.png'),
-        pygame.image.load('assets/personajes/ash/derechaCaminando1.png'),
-        pygame.image.load('assets/personajes/ash/derechaCaminando2.png')
-]
-
-caminaIzquierda = [
-        pygame.image.load('assets/personajes/ash/izquierda.png'),
-        pygame.image.load('assets/personajes/ash/izquierdaCaminando1.png'),
-        pygame.image.load('assets/personajes/ash/izquierdaCaminando2.png')
-]
-
-caminaAbajo = [
-        pygame.image.load('assets/personajes/ash/frente.png'),
-        pygame.image.load('assets/personajes/ash/frenteCaminando.png'),
-]
-
-caminaArriba = [
-        pygame.image.load('assets/personajes/ash/reversa.png'),
-        pygame.image.load('assets/personajes/ash/reversaCaminando1.png'),
-        pygame.image.load('assets/personajes/ash/reversaCaminando2.png')
-]
-
-x=0
-px = 1280
-py = 720
-ancho = 40
-velocidad = 10
-
-# Control de FPS
+# Reloj
 reloj = pygame.time.Clock()
 
-# Variables dirección
-izquierda = False
-derecha = False
-arriba = False
-abajo = False
 
-# Pasos
-cuentaPasos = 0
+class Ash:
 
-# Movimiento
-def recargaPantalla():
-    global cuentaPasos
-    global x
+    # Sprites Ash
+    def __init__(self) -> None:
+        # Velocidad
+        self.velocidad: int = 10
 
-    PANTALLA.blit(fondo, (1280, 720))
-    
-    # Contador de pasos
-    if cuentaPasos + 1 >= 3:
-        cuentaPasos = 0
+        # Posicion X y Posicion Y del personaje
+        self.px: int = 600
+        self.py: int = 600
 
-    # Movimiento hacia la izquierda
-    if izquierda:
-        PANTALLA.blit(caminaIzquierda[cuentaPasos // 1], (int(px), int(py)))
-        cuentaPasos += 1
+        # Variables dirección
+        self.izquierda: bool = False
+        self.derecha: bool = False
+        self.abajo: bool = False
+        self.arriba: bool = False
 
-    # Movimiento hacia la derecha
-    elif derecha:
-        PANTALLA.blit(caminaDerecha[cuentaPasos // 1], (int(px), int(py)))
-        cuentaPasos += 1
+        # Ejecutar bucle
+        self.ejecutar = True
 
-    # Movimiento hacia arriba
-    elif arriba:
-        PANTALLA.blit(caminaArriba[cuentaPasos // 1], (int(px), int(py)))
-        cuentaPasos += 1
+        # Cuenta pasos
+        self.cuentaPasos: int = 0
 
-    # Movimiento hacia arriba
-    elif abajo:
-        PANTALLA.blit(caminaAbajo[cuentaPasos // 1], (int(px), int(py)))
-        cuentaPasos += 1        
+        # Sprites Movimientos
+        self.spriteIzquierda: tuple = [
+            pygame.image.load('assets/personajes/ash/izquierda.png'),
+            pygame.image.load('assets/personajes/ash/izquierdaCaminando1.png'),
+            pygame.image.load('assets/personajes/ash/izquierdaCaminando2.png')]
 
-    else:
-        PANTALLA.blit(ash,(int(px), int(py)))
+        self.spriteDerecha: tuple = [
+            pygame.image.load('assets/personajes/ash/derecha.png'),
+            pygame.image.load('assets/personajes/ash/derechaCaminando1.png'),
+            pygame.image.load('assets/personajes/ash/derechaCaminando2.png')]
 
-    pygame.display.update()
+        self.spriteAbajo: tuple = [
+            pygame.image.load('assets/personajes/ash/frente.png'),
+            pygame.image.load('assets/personajes/ash/frenteCaminando.png'),
+            pygame.image.load('assets/personajes/ash/frenteCaminando.png')]
 
-ejecuta = True
-
-# Bucle de acciones y controles
-while ejecuta:
+        self.spriteArriba: tuple = [
+            pygame.image.load('assets/personajes/ash/reversa.png'),
+            pygame.image.load('assets/personajes/ash/reversaCaminando1.png'),
+            pygame.image.load('assets/personajes/ash/reversaCaminando2.png')]
         
-    # FPS
-    reloj.tick(18)
-
-    # Bucle del juego
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            ejecuta = False
-
-    # Opción tecla pulsada
-    keys = pygame.key.get_pressed()
-
-    # Tecla A - Movimiento a la izquierda
-    if keys[pygame.K_a]:
-        px -= velocidad
-        izquierda = True
-        derecha = False
-        arriba = False
-        abajo = False
-
-    # Tecla D - Movimiento a la derecha
-    elif keys[pygame.K_d]:
-        px += velocidad
-        izquierda = False
-        derecha = True
-        arriba = False
-        abajo = False
-
-    # Personaje quieto
-    else:
-        izquierda = False
-        derecha = False
-        arriba = False
-        abajo = True
-        cuentaPasos = 0
-
-    # Tecla W - Movimiento hacia arriba
-    if keys[pygame.K_w]:
-        izquierda = False
-        derecha = False
-        arriba = True
-        abajo = False
-        py -= velocidad
-
-    # Tecla S - Movimiento hacia abajo
-    if keys[pygame.K_s]:
-        izquierda = False
-        derecha = False
-        arriba = False
-        abajo = True
-        py += velocidad
+        self.spriteQuieto: str = pygame.image.load('assets/personajes/ash/frente.png')
 
 
-    # Actualización de la ventana
-    
-    PANTALLA.blit(fondo, (0, 0))
-    pygame.display.flip()
+    def getVelocidad(self) -> int:
+        return self.velocidad
 
-    # Llamada a la función de actualización de la ventana
-    recargaPantalla()
+    def setVelocidad(self, velocidad: int):
+        self.velocidad = velocidad
 
-# Salida del juego
-pygame.quit()
+    # Get y Set de PX y PY
+    def getPx(self) -> int:
+        return self.px
+
+    def setPx(self, px: int):
+        self.px = px
+
+    def getPy(self) -> int:
+        return self.py
+
+    def setPy(self, py: int):
+        self.py = py
+
+    # Get y Set movimientos
+    def getIzquierda(self) -> bool:
+        return self.izquierda
+
+    def setIzquierda(self, izquierda: bool):
+        self.izquierda = izquierda
+
+    def getDerecha(self) -> bool:
+        return self.derecha
+
+    def setDerecha(self, derecha: bool):
+        self.derecha = derecha
+
+    def getAbajo(self) -> bool:
+        return self.abajo
+
+    def setAbajo(self, abajo: bool):
+        self.abajo = abajo
+
+    def getArriba(self) -> bool:
+        return self.arriba
+
+    def setArriba(self, arriba: bool):
+        self.arriba = arriba
+
+    # Get de los sprites
+    def getSpriteIzquierda(self) -> tuple:
+        return self.spriteIzquierda
+
+    def getSpriteDerecha(self) -> tuple:
+        return self.spriteDerecha
+
+    def getSpriteAbajo(self) -> tuple:
+        return self.spriteAbajo
+
+    def getSpriteArriba(self) -> tuple:
+        return self.spriteArriba
+
+    def getSpriteQuieto(self) -> str:
+        return self.spriteQuieto
+
+    def getEjecutar(self) -> bool:
+        return self.ejecutar    
+
+    def getCuentaPasos(self) -> int:
+        return self.cuentaPasos
+
+    def setCuentaPasos(self, cuentaPasos: int):
+        self.cuentaPasos = cuentaPasos
+
+    def contadorDePasos(self) -> int:
+        # Contador de pasos
+        if (self.getCuentaPasos() + 1) >= 3:
+            self.setCuentaPasos(0)
+
+    def movimiento(self):
+
+        global fondo
+        PANTALLA.blit(fondo, (1280, 720))
+
+        # Movimiento hacia la izquierda
+        if self.getIzquierda():
+            PANTALLA.blit(self.getSpriteIzquierda[self.contadorDePasos()], (self.getPx(), self.getPy()))
+            self.setCuentaPasos(self.getCuentaPasos() + 1)
+
+        # Movimiento hacia la derecha
+        elif self.getDerecha():
+            PANTALLA.blit(self.getSpriteDerecha[self.contadorDePasos()], (self.getPx(), self.getPy()))
+            self.setCuentaPasos(self.getCuentaPasos() + 1)
+
+        # Movimiento hacia arriba
+        elif self.getArriba():
+            PANTALLA.blit(self.getSpriteArriba[self.contadorDePasos()], (self.getPx(), self.getPy()))
+            self.setCuentaPasos(self.getCuentaPasos() + 1)
+
+        # Movimiento hacia arriba
+        elif self.getAbajo():
+            PANTALLA.blit(self.getSpriteAbajo[self.contadorDePasos()], (self.getPx(), self.getPy()))
+            self.setCuentaPasos(self.getCuentaPasos() + 1) 
+
+        else:
+            PANTALLA.blit(self.getSpriteQuieto(), (self.getPx(), self.getPy()))
+
+        pygame.display.update()
+
+    # Bucle de acciones y controles
+    def ejecutarMovimiento(self):
+        while self.getEjecutar():
+
+            # Control de FPS
+            reloj.tick(18)
+
+            # Bucle del juego
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    # Salida del juego
+                    pygame.quit()
+
+            # Tecla pulsada
+            keys = pygame.key.get_pressed()
+
+            # Tecla A - Movimiento a la izquierda
+            if keys[pygame.K_a]:
+                self.setIzquierda(True)
+                self.setDerecha(False)
+                self.setArriba(False)
+                self.setAbajo(False)
+                i = (self.getPx()) - self.velocidad
+                self.setPx(i) 
+
+            # Tecla D - Movimiento a la derecha
+            elif keys[pygame.K_d]:
+                self.setIzquierda(False)
+                self.setDerecha(True)
+                self.setArriba(False)
+                self.setAbajo(False)
+                i = (self.getPx()) + self.velocidad
+                self.setPx(i) 
+
+            # Tecla W - Movimiento hacia arriba
+            elif keys[pygame.K_w]:
+                self.setIzquierda(False)
+                self.setDerecha(False)
+                self.setArriba(True)
+                self.setAbajo(False)
+                i = (self.getPy()) + self.velocidad
+                self.setPx(i)
+
+            # Tecla S - Movimiento hacia abajo
+            elif keys[pygame.K_s]:
+                self.setIzquierda(False)
+                self.setDerecha(False)
+                self.setArriba(False)
+                self.setAbajo(True)
+                i = (self.getPy()) - self.velocidad
+                self.setPx(i)
+
+            # Personaje quieto
+            else:
+                self.setIzquierda(False)
+                self.setDerecha(False)
+                self.setArriba(False)
+                self.setAbajo(False)
+                self.setCuentaPasos(0)
+
+            # Actualización de la ventana   
+            self.movimiento()
+            pygame.display.update()
+
+
+ash = Ash().ejecutarMovimiento()
