e012ff6df5fb95a53eaedeb2711d80e54053a7eb
c1t1gz <joaquinpoggicolman@gmail.com>
2021-09-06 17:50:43 -0300

    bash mas avanzado

--- Ultramon.sh
+++ Ultramon.sh
@@ -10,13 +10,13 @@ sudo apt install python3.9-venv -y
 
 sudo apt install python3-pip -y
 
-python3 -m venv venv
+python3.9 -m venv venv
 
 source venv/bin/activate
 
 python3 -m pip install -r requirements.txt
 
-python3 main.py
+python3.9 main.py
 
 
 
