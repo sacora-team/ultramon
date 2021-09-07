7903c344dd0a359e5b96a033f2faa9adf64927a3
joaquin <55362749+C1t1gz@users.noreply.github.com>
2021-09-07 11:09:00 -0300

    ultramon linux arreglado

--- UltramonLinux.sh
+++ UltramonLinux.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-<<<<<<< HEAD
-=======
-source InstaladorLinux.sh
-
-cd /home/$varname/$carpetaDelJuego/ultramon/
-
->>>>>>> e2d11495bde8704504385d269b132a6f7af83179
 python3.9 -m venv venv
 
 source venv/bin/activate
