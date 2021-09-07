494ac614ea45ea28288a399be97901b5201b9351
c1t1gz <joaquin15poggi@gmail.com>
2021-09-07 09:12:23 -0300

    Separacion ejecutable-instalador

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM python:3.9
-
-ADD main.py . 
-
-CMD ["./installWindows.ps1"]
---
+++ UltramonWindows.ps1
@@ -0,0 +1,7 @@
+﻿python -m venv venv
+
+venv\Scripts\activate
+
+python -m pip install -r requirements.txt
+
+python main.py
