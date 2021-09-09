943390d2305260f5c893df8a8e184b6523c9cff9
feehx25 <49326819+feehx25@users.noreply.github.com>
2021-09-08 21:09:37 -0300

    Mapa hecho con celdas, pasto y tierra

--- src/entidades/juego/Mapa.py
+++ src/entidades/juego/Mapa.py
@@ -1,67 +1,70 @@
 import pygame
- 
+
+ANCHO = pygame.display.Info().current_w
+ALTO = pygame.display.Info().current_h
+
 NEGRO = (0, 0, 0)
 BLANCO = (255, 255, 255)
 VERDE = ( 0, 255, 0)
 ROJO = (255, 0, 0)
 
+PASTO = pygame.image.load("assets/relieves/pasto.png")
+PASTO = pygame.transform.scale(PASTO, (30, 30))
+TIERRA = pygame.image.load("assets/relieves/tierra.png")
+TIERRA = pygame.transform.scale(TIERRA, (30, 30))
+
 LARGO  = 30
 ALTO = 30
 MARGEN = 1
  
-
 grid = []
 for fila in range(100):
     grid.append([])
     for columna in range(100):
-        grid[fila].append(0)
-
+        grid[fila].append(1)
 grid[0][0] = 1
- 
 
-pygame.init()
-  
+pygame.init()  
 
-DIMENSION_VENTANA = [1280, 720]
+DIMENSION_VENTANA = [ANCHO, ALTO]
 pantalla = pygame.display.set_mode(DIMENSION_VENTANA)
 pygame.display.set_caption("Mapa")
 
-hecho = False
-
+finalizar = True
 reloj = pygame.time.Clock()
 
-
-while not hecho:
+while finalizar:
     for evento in pygame.event.get(): 
         if evento.type == pygame.QUIT: 
-            hecho = True
-        elif evento.type == pygame.MOUSEBUTTONDOWN:
-            
-            pos = pygame.mouse.get_pos()
+            finalizar = False
 
+        elif evento.type == pygame.MOUSEBUTTONDOWN:
+            pos = pygame.mouse.get_pos()
             columna = pos[0] // (LARGO + MARGEN)
             fila = pos[1] // (ALTO + MARGEN)
-            
             grid[fila][columna] = 1
-            print("Click ", pos, "Coordenadas de la retícula: ", fila, columna)
+            
+        elif evento.type == pygame.MOUSEBUTTONUP:
+            pos = pygame.mouse.get_pos()
+            columna = pos[0] // (LARGO + MARGEN)
+            fila = pos[1] // (ALTO + MARGEN)
+            grid[fila][columna] = 0
 
 
     pantalla.fill(NEGRO)
- 
+
     for fila in range(100):
         for columna in range(100):
-            color = BLANCO
             if grid[fila][columna] == 1:
-                color = VERDE
-            pygame.draw.rect(pantalla,
-                             color,
-                             [(MARGEN + LARGO) * columna + MARGEN,
-                              (MARGEN + ALTO) * fila + MARGEN,
-                              LARGO,
-                              ALTO])
-     
-    reloj.tick(60)
+                pantalla.blit(PASTO, (((MARGEN + LARGO) * columna + MARGEN), ((MARGEN + ALTO) * fila + MARGEN)))
+                PASTO_RECT = PASTO.get_rect()
+                pantalla.blit(PASTO, PASTO_RECT)
+            elif grid[fila][columna] == 0:
+                pantalla.blit(TIERRA, (((MARGEN + LARGO) * columna + MARGEN), ((MARGEN + ALTO) * fila + MARGEN)))
+                TIERRA_RECT = TIERRA.get_rect()
+                pantalla.blit(TIERRA, TIERRA_RECT)
+
+    reloj.tick(100)
     pygame.display.flip()
-     
 
 pygame.quit()
