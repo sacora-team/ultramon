3ec895c732342582aebe0acaa30c80df161dfc80
galobponce <personal.galo.bello@gmail.com>
2021-09-11 17:03:08 -0300

    arreglo scripts

---
+++ runLinux.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+python3 -m venv venv
+
+source venv/bin/activate
+
+python3 -m pip install -r requirements.txt
+
+python3 main.py
---
+++ runWindows.ps1
@@ -0,0 +1,7 @@
+﻿py -m venv venv
+
+venv\Scripts\activate
+
+py -m pip install -r requirements.txt
+
+py main.py
