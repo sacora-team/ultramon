0c77e83e8b29c855008bb272e74998b467056992
Lautaro Viola <lviola@alumno.huergo.edu.ar>
2021-09-11 18:22:00 -0300

    Algunos cambios

--- src/entidades/juego/Mapa.py
+++ src/entidades/juego/Mapa.py
@@ -1,27 +1,22 @@
 import pygame
 
 from Bloque import Bloque
-from Pantalla import Pantalla
+from Pantalla import PANTALLA
 
 pygame.init()
-
-NEGRO = (0, 0, 0)
-BLANCO = (255, 255, 255)
-VERDE = ( 0, 255, 0)
-ROJO = (255, 0, 0)
 
 class Mapa:
 
     def __init__(self):
-        self.monitor = Pantalla()
+        self.pantalla = PANTALLA
         self.bloque = Bloque()
 
     def calcular_filas(self):
-        cantidad_filas = Pantalla.get_ancho_pantalla() // Bloque.get_ancho_bloque()
+        cantidad_filas = PANTALLA.get_ancho_pantalla() // Bloque.get_ancho_bloque()
         return cantidad_filas
 
     def calcular_columnas(self):
-        cantidad_columnas = Pantalla.get_alto_pantalla() // Bloque.get_alto_bloque()
+        cantidad_columnas = PANTALLA.get_alto_pantalla() // Bloque.get_alto_bloque()
         return cantidad_columnas
 
     grid = []
@@ -31,9 +26,7 @@ class Mapa:
             grid[fila].append(1)
     grid[0][0] = 1
 
-
-    PANTALLA = Pantalla().dimension_pantalla()
-    pygame.display.set_mode(PANTALLA)
+    pygame.display.set_mode(PANTALLA.get_ancho_pantalla, PANTALLA.get_alto_pantalla)
     pygame.display.set_caption("Ultramon")
 
     bucle = True
@@ -41,7 +34,7 @@ class Mapa:
 
     while bucle:
         pygame.mouse.set_visible(not bucle)
-        PANTALLA.fill(NEGRO)
+        PANTALLA.get_color_fondo()
 
         for fila in range(calcular_filas()*2):
             for columna in range(calcular_columnas()*2):
@@ -49,7 +42,8 @@ class Mapa:
                 if grid[fila][columna] == 1:
                     PANTALLA.blit(Bloque.PASTO, (
                         ((Bloque.get_margen_bloque() + Bloque.get_ancho_bloque) * columna + Bloque.get_margen_bloque()),
-                        ((Bloque.get_margen_bloque() + Bloque.get_alto_bloque) * fila + Bloque.get_margen_bloque())))
+                        ((Bloque.get_margen_bloque() + Bloque.get_alto_bloque) * fila + Bloque.get_margen_bloque()))
+                        )
 
                     PASTO_RECT = Bloque.PASTO.get_rect()
                     PANTALLA.blit(Bloque.PASTO, PASTO_RECT)
@@ -57,7 +51,8 @@ class Mapa:
                 elif grid[fila][columna] == 2:
                     PANTALLA.blit(Bloque.ARBOL, (
                         ((Bloque.get_margen_bloque() + Bloque.get_ancho_bloque) * columna + Bloque.get_margen_bloque()), 
-                        ((Bloque.get_margen_bloque() + Bloque.get_alto_bloque) * fila + Bloque.get_margen_bloque())))
+                        ((Bloque.get_margen_bloque() + Bloque.get_alto_bloque) * fila + Bloque.get_margen_bloque()))
+                        )
 
                     ARBOL_RECT = Bloque.ARBOL.get_rect()
                     PANTALLA.blit(Bloque.ARBOL, ARBOL_RECT)
--- src/entidades/juego/Pantalla.py
+++ src/entidades/juego/Pantalla.py
@@ -1,9 +1,11 @@
 import pygame
+from constantes.colores import Colores
 
 class Pantalla:
     def __init__(self):
         self.ancho_pantalla = pygame.display.Info().current_w
         self.alto_pantalla = pygame.display.Info().current_h
+        self.color_fondo = pygame.fill(Colores.NEGRO)
 
     def get_ancho_pantalla(self):
         return self.ancho_pantalla
@@ -11,5 +13,10 @@ class Pantalla:
     def get_alto_pantalla(self):
         return self.alto_pantalla
 
+    def get_color_fondo(self):
+        return self.color_fondo
+
     def dimension_pantalla(self):
         return (self.get_ancho_pantalla(), self.get_alto_pantalla())
+
+PANTALLA = Pantalla()
