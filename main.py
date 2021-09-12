0b55b16cfcdb22c0de85563819d9bfba1541d410
galobponce <personal.galo.bello@gmail.com>
2021-09-12 01:02:05 -0300

    ctypes-windll module not working on non-windows os

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from src.AppController import AppController
 from platform import system
-from ctypes import windll
+import ctypes
 from src.constantes.sistemasOperativos import SistemasOperativos
 
 sistema = system()
@@ -8,7 +8,7 @@ sistema = system()
 # Se cambia el id de la aplicacion para que windows no superponga el del juego
 if sistema == SistemasOperativos.WINDOWS:
     appid = u"ultramon"
-    windll.shell32.SetCurrentProcessExplicitAppUserModelID(appid)
+    ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(appid)
 
 
 if __name__ == "__main__":
