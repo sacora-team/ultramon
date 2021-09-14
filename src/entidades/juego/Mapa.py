718d9da4b34d3b9176493b5dfa87f8dd32815095
laucha23 <laucha.viola@alumno.huergo.edu.ar>
2021-09-14 20:27:28 -0300

    Mapa escalada y empezando a poner bloques

--- src/entidades/juego/Mapa.py
+++ src/entidades/juego/Mapa.py
@@ -11,8 +11,8 @@ class Mapa:
         self.alto_bloque: int = 40
         self.margen_bloque: int = 1
 
-        self.cantidad_filas: int = self.ancho_pantalla // self.ancho_bloque
-        self.cantidad_columnas: int = self.alto_pantalla // self.alto_bloque
+        self.cantidad_filas: int = (self.ancho_pantalla // self.ancho_bloque)
+        self.cantidad_columnas: int = (self.alto_pantalla // self.alto_bloque)
 
         self.dimension_pantalla: tuple = [self.ancho_pantalla, self.alto_pantalla]
 
@@ -46,13 +46,18 @@ PASTO = pygame.image.load("assets/relieves/pasto.png")
 PASTO = pygame.transform.scale(PASTO, (mapa.get_ancho_bloque(), mapa.get_alto_bloque()))
 TIERRA = pygame.image.load("assets/relieves/tierra.png")
 TIERRA = pygame.transform.scale(TIERRA, (mapa.get_ancho_bloque(), mapa.get_alto_bloque()))
+AGUA = pygame.image.load("assets/relieves/agua.png")
+AGUA = pygame.transform.scale(AGUA, (mapa.get_ancho_bloque(), mapa.get_alto_bloque()))
+
 
 grid = []
-for fila in range(100):
+for fila in range(mapa.get_cantidad_columnas()):
     grid.append([])
-    for columna in range(100):
-        grid[fila].append(1)
+    for columna in range(mapa.get_cantidad_filas()):
+        grid[fila].append(0)
 grid[0][0] = 1
+
+grid[10][8] = 2
 
 pantalla = pygame.display.set_mode(mapa.get_dimension_pantalla())
 pygame.display.set_caption("Ultramon")
@@ -67,23 +72,34 @@ while bucle:
 
     pantalla.fill((0,0,0))
 
-    for fila in range(100):
-        for columna in range(100):
-            if grid[fila][columna] == 1:
+    for fila in range(mapa.get_cantidad_columnas()):
+        for columna in range(mapa.get_cantidad_filas()):
+            if grid[fila][columna] == 0:
                 pantalla.blit(PASTO, (
                     ((mapa.get_margen_bloque() + mapa.get_ancho_bloque()) * columna + mapa.get_margen_bloque()),
                     ((mapa.get_margen_bloque() + mapa.get_alto_bloque()) * fila + mapa.get_margen_bloque()))
                     )
+
                 PASTO_RECT = PASTO.get_rect()
                 pantalla.blit(PASTO, PASTO_RECT)
 
-            elif grid[fila][columna] == 0:
+            elif grid[fila][columna] == 1:
                 pantalla.blit(TIERRA, (
                     ((mapa.get_margen_bloque() + mapa.get_ancho_bloque()) * columna + mapa.get_margen_bloque()),
                     ((mapa.get_margen_bloque() + mapa.get_alto_bloque()) * fila + mapa.get_margen_bloque()))
                     )
+
                 TIERRA_RECT = TIERRA.get_rect()
                 pantalla.blit(TIERRA, TIERRA_RECT)
+
+            elif grid[fila][columna] == 2:
+                pantalla.blit(AGUA, (
+                    ((mapa.get_margen_bloque() + mapa.get_ancho_bloque()) * columna + mapa.get_margen_bloque()),
+                    ((mapa.get_margen_bloque() + mapa.get_alto_bloque()) * fila + mapa.get_margen_bloque()))
+                    )
+
+                AGUA_RECT = AGUA.get_rect()
+                pantalla.blit(AGUA, AGUA_RECT)
 
     reloj.tick(100)
     pygame.display.flip()
